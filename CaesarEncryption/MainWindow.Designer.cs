148f9c08edc9d7081f2580d2588f51d1ff420d55
Le Minh Tri <ansidev@gmail.com>
2014-06-25 03:02:15 +0700

    Updated code!

---
+++ CaesarEncryption/MainWindow.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CaesarEncryption
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.lblInput = new System.Windows.Forms.Label();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(12, 9);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(98, 14);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Input string:";
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxInput.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxInput.Location = new System.Drawing.Point(113, 6);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(479, 22);
+            this.tbxInput.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(12, 36);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(105, 14);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output string:";
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxOutput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbxOutput.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxOutput.Location = new System.Drawing.Point(113, 33);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.ReadOnly = true;
+            this.tbxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxOutput.Size = new System.Drawing.Size(560, 363);
+            this.tbxOutput.TabIndex = 5;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncrypt.Location = new System.Drawing.Point(598, 4);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "PROCESS";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 408);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Caesar Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+    }
+}
+
---
+++ CaesarEncryption/MainWindow.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaesarEncryption
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private string ReverseString(string s)
+        {
+            string result = "";
+            string temp = "";
+            foreach (char letter in s)
+            {
+                    temp = result;
+                    result = letter + temp;
+            }
+            return result;
+        }
+        private int LetterToNumber(char letter)
+        {
+            int number = -1;
+            switch(letter)
+            {
+                case 'a':
+                    number = 0;
+                    break;
+                case 'b':
+                    number = 1;
+                    break;
+                case 'c':
+                    number = 2;
+                    break;
+                case 'd':
+                    number = 3;
+                    break;
+                case 'e':
+                    number = 4;
+                    break;
+                case 'f':
+                    number = 5;
+                    break;
+                case 'g':
+                    number = 6;
+                    break;
+                case 'h':
+                    number = 7;
+                    break;
+                case 'i':
+                    number = 8;
+                    break;
+                case 'j':
+                    number = 9;
+                    break;
+                case 'k':
+                    number = 10;
+                    break;
+                case 'l':
+                    number = 11;
+                    break;
+                case 'm':
+                    number = 12;
+                    break;
+                case 'n':
+                    number = 13;
+                    break;
+                case 'o':
+                    number = 14;
+                    break;
+                case 'p':
+                    number = 15;
+                    break;
+                case 'q':
+                    number = 16;
+                    break;
+                case 'r':
+                    number = 17;
+                    break;
+                case 's':
+                    number = 18;
+                    break;
+                case 't':
+                    number = 19;
+                    break;
+                case 'u':
+                    number = 20;
+                    break;
+                case 'v':
+                    number = 21;
+                    break;
+                case 'w':
+                    number = 22;
+                    break;
+                case 'x':
+                    number = 23;
+                    break;
+                case 'y':
+                    number = 24;
+                    break;
+                case 'z':
+                    number = 25;
+                    break;
+                default:
+                    break;
+            }
+            return number;
+        }
+        private char NumberToLetter(int number)
+        {
+            char letter = ' ';
+            switch(number)
+            {
+                case 0:
+                    letter = 'a';
+                    break;
+                case 1:
+                    letter = 'b';
+                    break;
+                case 2:
+                    letter = 'c';
+                    break;
+                case 3:
+                    letter = 'd';
+                    break;
+                case 4:
+                    letter = 'e';
+                    break;
+                case 5:
+                    letter = 'f';
+                    break;
+                case 6:
+                    letter = 'g';
+                    break;
+                case 7:
+                    letter = 'h';
+                    break;
+                case 8:
+                    letter = 'i';
+                    break;
+                case 9:
+                    letter = 'j';
+                    break;
+                case 10:
+                    letter = 'k';
+                    break;
+                case 11:
+                    letter = 'l';
+                    break;
+                case 12:
+                    letter = 'm';
+                    break;
+                case 13:
+                    letter = 'n';
+                    break;
+                case 14:
+                    letter = 'o';
+                    break;
+                case 15:
+                    letter = 'p';
+                    break;
+                case 16:
+                    letter = 'q';
+                    break;
+                case 17:
+                    letter = 'r';
+                    break;
+                case 18:
+                    letter = 's';
+                    break;
+                case 19:
+                    letter = 't';
+                    break;
+                case 20:
+                    letter = 'u';
+                    break;
+                case 21:
+                    letter = 'v';
+                    break;
+                case 22:
+                    letter = 'w';
+                    break;
+                case 23:
+                    letter = 'x';
+                    break;
+                case 24:
+                    letter = 'y';
+                    break;
+                case 25:
+                    letter = 'z';
+                    break;
+                default:
+                    break;
+            }
+            return letter;
+        }
+        private string CaesarCrypt(int key)
+        {
+            string input = this.tbxInput.Text;
+            string output = "";
+            //MessageBox.Show(s);
+            foreach (char letter in input)
+            {
+                output += NumberToLetter((LetterToNumber(letter) + key) % 26);
+            }
+            return output;
+        }
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            this.tbxOutput.Text = "";
+            for (int i = 1; i < 26; i++)
+            {
+                this.tbxOutput.Text += "k = " + i + ":\t" + CaesarCrypt(i) + "\t" + ReverseString(CaesarCrypt(i)) + "\r\n";
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
